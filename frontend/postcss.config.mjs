c5d5630733cfd976530ec8a7542c83b16c61a2dc
raj-patel-149 <rdpatel11124@gmail.com>
2025-07-04 15:41:32 +0530

    modify next.config.mjs file

--- frontend/postcss.config.mjs
+++ frontend/postcss.config.mjs
@@ -1,8 +1,6 @@
-const config = {
+module.exports = {
   plugins: {
-    tailwindcss: {},
+    "@tailwindcss/postcss": {},
     autoprefixer: {},
   },
 };
-
-export default config;
--- frontend/tailwind.config.mjs
+++ frontend/tailwind.config.mjs
@@ -1,14 +1,7 @@
-/** @type {import('tailwindcss').Config} */
-const config = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
 };
-
-export default config;
